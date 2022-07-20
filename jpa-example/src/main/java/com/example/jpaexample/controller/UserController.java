35cd9b0456badd06d205e198d4dfc32a7bc2d314
lin <linkq@dreamdt.cn>
2022-07-20 17:10:16 +0800

    1.增加PagingRepository接口 api测试 sort和page方法 2.当我们用任何第三方方法的时候，最好先查一下其源码和逻辑或者 API，然后再写出优雅的代码。 第一，上面的 7 个大 Repository 接口，我们在使用的时候可以根据实际场景，来继承不同 的接口，从而选择暴露不同的 Spring Data Common 给我们提供的已有接口。这其实利用了 Java 语言的 interface 特性，在这里可以好好理解一下 interface 的妙用。 第二，利用源码也可以很好地理解一下 Spring 中动态代理的作用，可以利用这种思想，在 改善 MyBatis 的时候使用。

--- jpa-example/src/main/java/com/example/jpaexample/controller/UserController.java
+++ jpa-example/src/main/java/com/example/jpaexample/controller/UserController.java
@@ -5,7 +5,9 @@ import com.example.jpaexample.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,6 +34,8 @@ public class UserController {
     @PostMapping(path = "user",consumes =
             {MediaType.APPLICATION_JSON_VALUE})
     public User addNewUser(@RequestBody User user) {
+        // 自己在做 Save 的时候先去 Find 一下，其实是没有必要的，Spring JPA 底层都考虑到了。
+        // TODO 这里其实是想告诉你，当我们用任何第三方方法的时候，最好先查一下其源码和逻辑或者 API，然后再写出优雅的代码。
         return userRepository.save(user);
     }
     /**
@@ -43,6 +47,27 @@ public class UserController {
     @ResponseBody
     public Page<User> getAllUsers(Pageable request) {
         return userRepository.findAll(request);
+    }
+    /**
+     * 验证排序和分页查询方法，Pageable的默认实现类：PageRequest
+     * @return
+     */
+    @GetMapping(path = "/page")
+    @ResponseBody
+    public Page<User> getAllUserByPage() {
+        return userRepository.findAll(
+                PageRequest.of(1, 20, Sort.by(new
+                        Sort.Order(Sort.Direction.ASC,"name"))));
+    }
+    /**
+     * 排序查询方法，使用Sort对象
+     * @return
+     */
+    @GetMapping(path = "/sort")
+    @ResponseBody
+    public Iterable<User> getAllUsersWithSort() {
+        return userRepository.findAll(Sort.by(new
+                Sort.Order(Sort.Direction.ASC,"name")));
     }
 }
 
